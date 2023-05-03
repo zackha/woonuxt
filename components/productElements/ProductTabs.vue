c813baf77ffd27cb3b8e3b8c2fb17befcda6b47b
Zack Hatlen <zckhtln@icloud.com>
2023-05-03 13:35:09 +0300

    Update ProductTabs.vue

--- components/productElements/ProductTabs.vue
+++ components/productElements/ProductTabs.vue
@@ -20,19 +20,23 @@ const show = ref(0);
             <ReviewsScore :reviews="product.reviews" :productId="product.databaseId" />
           </div>
           <div class="divide-y flex-1">
-            <div v-for="review in product.reviews.edges" :key="review.id" class="my-2 py-8">
-              <div class="flex gap-4 items-center">
-                <img v-if="review.node.author.node.avatar" :src="review.node.author.node.avatar.url" class="rounded-full h-12 w-12" />
-                <div class="grid gap-1">
-                  <div class="text-sm">
-                    <span class="font-semibold">{{ review.node.author.node.name }}</span>
-                    <span class="italic text-gray-400"> – {{ new Date(review.node.date).toLocaleString($t('messages.general.langCode'), { month: 'long', day: 'numeric', year: 'numeric' }) }}</span>
+            <ClientOnly fallback-tag="span" fallback="Loading comments...">
+              <div v-for="review in product.reviews.edges" :key="review.id" class="my-2 py-8">
+                <div class="flex gap-4 items-center">
+                  <img v-if="review.node.author.node.avatar" :src="review.node.author.node.avatar.url" class="rounded-full h-12 w-12" />
+                  <div class="grid gap-1">
+                    <div class="text-sm">
+                      <span class="font-semibold">{{ review.node.author.node.name }}</span>
+                      <span class="italic text-gray-400"
+                        >– {{ new Date(review.node.date).toLocaleString($t('messages.general.langCode'), { month: 'long', day: 'numeric', year: 'numeric' }) }}</span
+                      >
+                    </div>
+                    <StarRating :rating="review.rating" :hide-count="true" class="text-sm" />
                   </div>
-                  <StarRating :rating="review.rating" :hide-count="true" class="text-sm" />
                 </div>
+                <div class="mt-4 text-gray-700 italic prose-sm" v-html="review.node.content"></div>
               </div>
-              <div class="mt-4 text-gray-700 italic prose-sm" v-html="review.node.content"></div>
-            </div>
+            </ClientOnly>
           </div>
         </div>
       </div>
